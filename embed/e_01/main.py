89ced1a9241eb55a2bd5f5005a8f21e2bd53adb9
chronotriggered <122747776+chronotriggered@users.noreply.github.com>
2025-09-14 21:18:29 +0300

    exercises 1, 2, 3, 4, 5, 6, 7

--- embed/e_01/main.py
+++ embed/e_01/main.py
@@ -1,62 +1,11 @@
-import utime
-import machine
+import time
+time.sleep(0.1)
 
-punainen_valo = False
-last_press_time = 0
+for i in range(10):
+    print(i)
 
-led_red = machine.Pin(15, machine.Pin.OUT)
-led_yellow = machine.Pin(14, machine.Pin.OUT)
-led_green = machine.Pin(13, machine.Pin.OUT)
-buzzer = machine.Pin(12, machine.Pin.OUT)
-
-def interrupt_handler(pin):
-    global punainen_valo, last_press_time
-    now = utime.ticks_ms()
-    if utime.ticks_diff(now, last_press_time) > 300:
-        punainen_valo = True
-        last_press_time = now
-
-button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
-button.irq(trigger=machine.Pin.IRQ_RISING, handler=interrupt_handler)
-
-def stop_traffic():
-    led_red.value(1)
-    for i in range(10):
-        buzzer.value(1)
-        utime.sleep(0.2)
-        buzzer.value(0)
-        utime.sleep(0.2)
-    led_red.value(0)
-
-while True:
-    if punainen_valo:
-        stop_traffic()
-        punainen_valo = False
-
-    led_red.value(1)
-    for _ in range(20):
-        utime.sleep(0.1)
-        if punainen_valo:
-            break
-    led_red.value(0)
-
-    led_yellow.value(1)
-    for _ in range(20):
-        utime.sleep(0.1)
-        if punainen_valo:
-            break
-    led_yellow.value(0)
-
-    led_green.value(1)
-    for _ in range(50):
-        utime.sleep(0.1)
-        if punainen_valo:
-            break
-    led_green.value(0)
-
-    led_yellow.value(1)
-    for _ in range(20):
-        utime.sleep(0.1)
-        if punainen_valo:
-            break
-    led_yellow.value(0)
+name = input("What is your name?")
+if name == "Clark Kent":
+    print("You are the Superman!")
+else:
+    print("You are an ordinary person.")
--- embed/e_06/main.py
+++ embed/e_06/main.py
@@ -1,62 +1,22 @@
-import utime
 import machine
+import utime
+import urandom
 
-punainen_valo = False
-last_press_time = 0
+start = 0
 
-led_red = machine.Pin(15, machine.Pin.OUT)
-led_yellow = machine.Pin(14, machine.Pin.OUT)
-led_green = machine.Pin(13, machine.Pin.OUT)
-buzzer = machine.Pin(12, machine.Pin.OUT)
+led = machine.Pin(0, machine.Pin.OUT)
+btn = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
 
-def interrupt_handler(pin):
-    global punainen_valo, last_press_time
-    now = utime.ticks_ms()
-    if utime.ticks_diff(now, last_press_time) > 300:
-        punainen_valo = True
-        last_press_time = now
+def btnHandler(pin):
+    btn.irq(handler=None)
+    reaction = (utime.ticks_diff(utime.ticks_ms(), start) / 1000)
+    print("Your reaction time was " + str(reaction) + " seconds")
 
-button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
-button.irq(trigger=machine.Pin.IRQ_RISING, handler=interrupt_handler)
 
-def stop_traffic():
-    led_red.value(1)
-    for i in range(10):
-        buzzer.value(1)
-        utime.sleep(0.2)
-        buzzer.value(0)
-        utime.sleep(0.2)
-    led_red.value(0)
+led.value(1)
+utime.sleep(urandom.uniform(3, 10))
 
-while True:
-    if punainen_valo:
-        stop_traffic()
-        punainen_valo = False
+led.value(0)
+start = utime.ticks_ms()
 
-    led_red.value(1)
-    for _ in range(20):
-        utime.sleep(0.1)
-        if punainen_valo:
-            break
-    led_red.value(0)
-
-    led_yellow.value(1)
-    for _ in range(20):
-        utime.sleep(0.1)
-        if punainen_valo:
-            break
-    led_yellow.value(0)
-
-    led_green.value(1)
-    for _ in range(50):
-        utime.sleep(0.1)
-        if punainen_valo:
-            break
-    led_green.value(0)
-
-    led_yellow.value(1)
-    for _ in range(20):
-        utime.sleep(0.1)
-        if punainen_valo:
-            break
-    led_yellow.value(0)
+btn.irq(trigger=machine.Pin.IRQ_FALLING, handler=btnHandler)
